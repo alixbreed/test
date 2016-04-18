976b1e6614e58a8a1ce5e2620d7fe2b18e64ea41
alixbreed <alixbreed@gmail.com>
2016-04-18 09:39:33 +0300

    init

---
+++ xmloperator.cpp
@@ -0,0 +1,6 @@
+#include "xmloperator.h"
+
+XMLOperator::XMLOperator()
+{
+
+}
---
+++ xmloperator.h
@@ -0,0 +1,11 @@
+#ifndef XMLOPERATOR_H
+#define XMLOPERATOR_H
+
+
+class XMLOperator
+{
+public:
+    XMLOperator();
+};
+
+#endif // XMLOPERATOR_H
